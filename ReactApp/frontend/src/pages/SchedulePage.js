98e46102c226308067632335379db75c5ba216b3
RedPandaGA <103851430+RedPandaGA@users.noreply.github.com>
2022-12-02 11:20:03 -0600

    schedulepagev1

--- ReactApp/frontend/src/pages/SchedulePage.js
+++ ReactApp/frontend/src/pages/SchedulePage.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { DataGrid } from '@mui/x-data-grid';
-import { Box } from '@mui/material'
+import { Box, Select, MenuItem, FormControl, InputLabel, Button } from '@mui/material'
 import Navbar from './navbar'
 
 function SchedulePage(){
     var rowIndex = 0;
+    const [possibleDept, setPossibleDept] = useState([])
+    const [curDept, setCurDept] = useState("")
     const [schedule, setSchedule] = useState([])
     const [possible, setPossible] = useState([])
+    const [toadd, setToAdd] = useState([])
+    const [selected, setSelected] = useState([])
 
     useEffect(() => {
         //get users courses
@@ -34,7 +38,36 @@ function SchedulePage(){
             {dept: "CSC", CID: 4444, sect: 1, hrs: 3, begin: 1030, end: 1200, days: 'TTH', location: 'PFT'}
         ]
         setPossible(possibleCourses)
+        //get possible dept
+        var possibleDepts = ["CSC", "EE", "ENGL"]
+        setPossibleDept(possibleDepts)
     }, [])
+
+    const handlePossible = (e) => {
+        setToAdd(e.target.value)
+    }
+    
+    const handlePossibleDept = (e) => {
+        setCurDept(e.target.value)
+    }
+
+    const handleSelection = (e) => {
+        setSelected(e)
+    }
+
+    const handleAddto = (e) => {
+        var newschedule = [...schedule]
+        //console.log(toadd)
+        toadd.forEach((course) => {
+            course.id = newschedule[newschedule.length-1].id+1
+            newschedule.push(course)
+        })
+        setSchedule(newschedule)
+    }
+    //newschedule.splice(sel, 1)
+    const handleRemove = (e) => {
+        
+    }
 
     const columns = [
         {field: 'dept', headerName: 'Dept', width: 50},
@@ -44,7 +77,8 @@ function SchedulePage(){
         {field: 'begin', headerName: 'Begin Time', width: 100 },
         {field: 'end', headerName: 'End Time', width: 100 },
         {field: 'days', headerName: 'Days', width: 100 },
-        {field: 'location', headerName: 'Loaction', width: 100 }
+        {field: 'location', headerName: 'Loaction', width: 100 },
+        {field: 'prereq', headerName: 'Prerequirement(s)', width: 200}
     ]
 
 
@@ -56,10 +90,43 @@ function SchedulePage(){
                     rows={schedule}
                     columns={columns}
                     rowsPerPageOptions={[5,10,20,40]}
-                    autoHeight
+                    onSelectionModelChange={handleSelection}
                     checkboxSelection
-                    disableSelectionOnClick
+                    autoHeight
                 />
+                <FormControl sx={{m: 1, width: 100}}>
+                    <InputLabel>Dept</InputLabel>
+                    <Select
+                        value={curDept}
+                        label="Course"
+                        onChange={handlePossibleDept}
+                    >
+                        {possibleDept.map((Dept) => {
+                            return(
+                                <MenuItem value={Dept}>{Dept}</MenuItem>
+                            )
+                        })}
+                    </Select>
+                </FormControl>
+                <FormControl sx={{m: 1, width: 300}}>
+                    <InputLabel>Possible</InputLabel>
+                    <Select
+                        value={toadd}
+                        label="Course"
+                        onChange={handlePossible}
+                        multiple
+                    >
+                        {possible.map((possibleCourse) => {
+                            return(
+                                <MenuItem value={possibleCourse}>{possibleCourse.dept + possibleCourse.CID}</MenuItem>
+                            )
+                        })}
+                    </Select>
+                </FormControl>
+                <FormControl sx={{m: 1, width: 180}}>
+                    <Button variant="contained" onClick={handleAddto}>Add To Schedule</Button>
+                    <Button variant="contained" onClick={handleRemove}>Remove Selected</Button>
+                </FormControl>
             </Box>
         </div>
     )
