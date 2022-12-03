c4ada124e0725e421a2718c8baf699895404aca2
RedPandaGA <103851430+RedPandaGA@users.noreply.github.com>
2022-12-02 21:18:23 -0600

    temp fin

--- ReactApp/frontend/src/pages/home.js
+++ ReactApp/frontend/src/pages/home.js
@@ -6,8 +6,10 @@ function Home(){
     return(
         <div>
             <Navbar/>
-            Hello World
+            <div>Go to Schedule to view current schedule and add courses or select a premade schedule.</div>
+            <div>Go to Flowchart to view and edit a flowchart or load a premade one.</div>
         </div>
+
     )
 }
 
