86cdd82e3a5f4542807e1bf92ac39a5ee100d221
RedPandaGA <103851430+RedPandaGA@users.noreply.github.com>
2022-12-02 19:30:16 -0600

    flowchart getting schedule

--- ReactApp/frontend/src/pages/FlowchartPage.js
+++ ReactApp/frontend/src/pages/FlowchartPage.js
@@ -3,67 +3,67 @@ import ReactFlow, { Controls, Background, useNodesState, useEdgesState, updateEd
 import { Box } from '@mui/material'
 import 'reactflow/dist/style.css'
 import './flowchart.css'
+import axios from 'axios'
 import Navbar from './navbar'
 
 
+const getScheduleURL = process.env.NODE_ENV === 'production' ? '/getUserSchedule' : 'http://localhost:3002/api/getUserSchedule'
 
 function FlowchartPage(){
-    
-    var rows = [
-            {dept: "CSC", CID: '1350', hrs: 3 },
-            {dept: "CSC", CID: '1351', hrs: 3 },
-            {dept: "CSC", CID: '3102', hrs: 3 },
-            {dept: "CSC", CID: '2259', hrs: 3 },
-            {dept: "CSC", CID: '2262', hrs: 3 },
-            {dept: "CSC", CID: '3380', hrs: 3 },
-            {dept: "CSC", CID: '3501', hrs: 3 },
-            {dept: "CSC", CID: '4330', hrs: 3 },
-            // {dept: "CSC", CID: '2000+', hrs: 3 },
-            // {dept: "CSC", CID: '2000+', hrs: 3 },
-            {dept: "CSC", CID: '4103', hrs: 3 },
-            {dept: "CSC", CID: '4101', hrs: 3 },
-            {dept: "CSC", CID: '3200', hrs: 3 },
-            // {dept: "CSC", CID: '4000+', hrs: 3 },
-            // {dept: "CSC", CID: '4000+', hrs: 3 },
-            // {dept: "CSC", CID: '3000+', hrs: 3 },
-            {dept: "MATH", CID: '1550', hrs: 3 },
-            {dept: "MATH", CID: '1552', hrs: 3 },
-            {dept: "MATH", CID: '2090', hrs: 3 },
-            {dept: "IE", CID: '3302', hrs: 3 },
-            {dept: "ENGL", CID: '1001', hrs: 3 },
-            // {dept: "ENGL", CID: 'GENED', hrs: 3 },
-            {dept: "ENGL", CID: '2000', hrs: 3 },
-            // {dept: "CMST", CID: 'GENED', hrs: 3 },
-    ]
+    const token = JSON.parse(localStorage.getItem('udata')).token
+    const uid = JSON.parse(localStorage.getItem('udata')).userId
+
+    //const [newnodes, setNewNodes] = useState([])
+    const [schedule, setSchedule] = useState([])
+    const flowKey = 'example-flow';
+    const [nodes, setNodes, onNodesChange] = useNodesState([]);
+    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+    const [rfInstance, setRfInstance] = useState(null);
+    const edgeUpdateSuccessful = useRef(true);
+
+    async function getUserSchedule(){
+        var rows = []
+        await axios({
+            method: 'GET',
+            url: getScheduleURL,
+            headers: { Authorization: `token ${token}` },
+            params: { uid: uid }
+        })
+        .then((res) => {
+            rows = res.data.rows[0].scheduledata
+        })
+        .catch((err) => {
+            console.log(err)
+            alert("Failed to fetch schedule. Try again later")
+        })
+        if(rows.length > 0){
+            setSchedule(rows)
+        }
+    }
 
     useEffect(() => {
-        
+        getUserSchedule()
     }, [])
 
-    var initialNodes = []
-
     useEffect(() => {
-        var numNodes = 0;
-        rows.forEach((course) => {
+        var numNodes = 0
+        var newnodes = []
+        schedule.forEach((course) => {
             let newNode = {}
-            let courseName = course.dept + course.CID
+            let courseName = course.dept + course.cid
             newNode.id = (courseName)
             newNode.data = { label: courseName }
             let newPositionX = numNodes * 150
             console.log(newPositionX)
             newNode.position = { x: newPositionX, y: 0 }
             numNodes++
-            initialNodes.push(newNode)
+            newnodes.push(newNode)
         })
-    }, [])
+        console.log(newnodes)
+        setNodes(newnodes)
+        console.log(nodes)
+    }, [schedule])
 
-    const initialEdges = [];
-
-    const flowKey = 'example-flow';
-    const [nodes, , onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    const [rfInstance, setRfInstance] = useState(null);
-    const edgeUpdateSuccessful = useRef(true);
     const onConnect = useCallback((params) => setEdges((els) => addEdge(params, els)), []);
     
     const onEdgeUpdateStart = useCallback(() => {
